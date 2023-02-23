4a1bbc604d8509bf917e3eee87cc1d9ec7d1afa7
sir-daxis <dawid.gawelski@gmail.com>
2023-02-23 15:39:43 +0100

    edit models.py and add data to databse

--- src/wordbase/admin.py
+++ src/wordbase/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Word
+from .models import Word, Person, Plural, Form, Past, AppUser, WordClass
 
 # Register your models here.
 admin.site.register(Word)
+admin.site.register(AppUser)
+admin.site.register(WordClass)
+admin.site.register(Person)
+admin.site.register(Plural)
+admin.site.register(Form)
+admin.site.register(Past)
--- src/wordbase/models.py
+++ src/wordbase/models.py
@@ -2,29 +2,57 @@ from django.db import models
 
 
 # Create your models here.
+class AppUser(models.Model):
+    user_name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return ''.join([self.user_name, f' [{self.pk}]'])
+
+
+class WordClass(models.Model):
+    word_type = models.CharField(max_length=15)
+
+    def __str__(self):
+        return ''.join([self.word_type, f' [{self.pk}]'])
+
+
+class Person(models.Model):
+    personality = models.CharField(max_length=3)
+
+    def __str__(self):
+        return ''.join([self.personality, f' [{self.pk}]'])
+
+
 class Word(models.Model):
     name = models.CharField(max_length=50)
     meaning = models.CharField(max_length=50)
-    word_class = models.CharField(max_length=1)
-    personality = models.CharField(max_length=1)
-    sample = models.CharField(max_length=50)
-    polish_sample = models.CharField(max_length=50)
+    word_class = models.ForeignKey(WordClass, on_delete=models.CASCADE)
+    personality = models.ForeignKey(Person, on_delete=models.CASCADE, null=True, blank=True)
+    sample = models.CharField(max_length=150)
+    polish_sample = models.CharField(max_length=150)
     pub_date = models.DateTimeField()
-    last_question = models.DateTimeField('date published')
+    last_question = models.DateTimeField()
     num_answer = models.IntegerField(default=0)
     num_correct = models.IntegerField(default=0)
     ratio_correct = models.FloatField(default=0)
-    user = models.IntegerField(default=0)
+    user = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return ''.join([self.name, f' [{self.pk}]'])
 
 
 class Form(models.Model):
     name = models.ForeignKey(Word, on_delete=models.CASCADE)
     ich = models.CharField(max_length=50)
     du = models.CharField(max_length=50)
-    er = models.CharField(max_length=50)
+    er_es_sie = models.CharField(max_length=50)
     wir = models.CharField(max_length=50)
     ihr = models.CharField(max_length=50)
-    sie = models.CharField(max_length=50)
+    sie_sie = models.CharField(max_length=50)
+
+    def __str__(self):
+        return ''.join([self.sie_sie, f' [{self.pk}]'])
 
 
 class Past(models.Model):
@@ -32,11 +60,6 @@ class Past(models.Model):
     past_tense = models.CharField(max_length=50)
 
 
-class Person(models.Model):
-    name = models.ForeignKey(Word, on_delete=models.CASCADE)
-    personality = models.CharField(max_length=1)
-
-
-class Prular(models.Model):
+class Plural(models.Model):
     name = models.ForeignKey(Word, on_delete=models.CASCADE)
     personality = models.CharField(max_length=50)
