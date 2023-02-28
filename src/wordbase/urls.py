8b4a132a7b8af44f2f2de288d7bb0faaad5c7f43
sir-daxis <dawid.gawelski@gmail.com>
2023-02-28 20:41:36 +0100

    add test and reply templates and views, first working prototype

--- src/wordbase/urls.py
+++ src/wordbase/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('index/', views.index, name='index'),
     path('add_word/', views.add_word, name='add_word'),
-    path('int:word_id/test', views.test, name='test'),
-    path('int:word_id/edit', views.edit_word, name='edit_word'),
+    path('run_test/', views.run_test, name='run_test'),
+    path('run_test/<int:word_id>/test', views.test, name='test'),
+    path('run_test/<int:word_id>/reply', views.reply, name='reply'),
+    path('<int:word_id>/edit', views.edit_word, name='edit_word'),
 ]
--- src/wordbase/views.py
+++ src/wordbase/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from random import randrange
 
 from .models import Word
 
@@ -24,5 +25,39 @@ def add_word(request):
 def edit_word(request):
     return HttpResponse('View: edit_word')
 
-def test(request):
-    return HttpResponse('View: test')
+
+def run_test(request):
+    id_list = Word.objects.values_list('pk', flat=True)
+    random_id = id_list[randrange(0, len(id_list))]
+    return HttpResponseRedirect(f'{Word.objects.get(pk=random_id).id}/test')
+
+
+def test(request, word_id):
+    word = get_object_or_404(Word, pk=word_id)
+    # return HttpResponse(f'View: test, word_id: {word_id}')
+    return render(request, 'wordbase/test.html', {'word': word})
+
+
+def reply(request, word_id):
+    word = get_object_or_404(Word, pk=word_id)
+    try:
+        answer = request.POST['answer']
+    except (KeyError, Word.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'wordbase/test.html', {
+            'word': word,
+            'error_message': "You didn't write a answer.",
+        })
+    else:
+        word.last_question = timezone.now()
+        word.num_answer += 1
+        correct_answer = False
+        if answer.upper() == word.name.upper():
+            correct_answer = True
+            word.num_correct += 1
+        word.save()
+        return render(request, 'wordbase/reply.html', {
+            'word': word,
+            'correct_answer': correct_answer,
+            'answer': answer,
+        })
